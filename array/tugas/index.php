4f1d85bbf08d0b01356e07ed3216166fa3c1bff8
rafamaritza <rafamaritza456@gmail.com>
2022-11-07 13:15:27 +0700

    update tugas

--- array/tugas/index.php
+++ array/tugas/index.php
@@ -7,9 +7,9 @@ $pren =['aliya','nazwa','caca','rafa','maritza'];
 echo $temen[0]," ",$pren[0]," ",$temen[1]," ",$pren[1]," ",$temen[2]," ",$pren[2]," ",$temen[3]," ",$pren[3]," ",$temen[4]," ",$pren[4];
 echo "<br />";
 
-//tambah bestie
-array_push($temen,'rahma');
-array_push($pren,'nadia');
+
+array_push($temen,'deswita');
+array_push($pren,'micel');
 
 echo $temen[5]," ",$pren[5];
 
