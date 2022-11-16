b8165a70a933a893d89e0df1e2fb44ba4f02db10
rafamaritza <rafamaritza456@gmail.com>
2022-11-16 09:55:03 +0700

    variabel

---
+++ variabel enviroment/file-01.php
@@ -0,0 +1,9 @@
+<?php
+// variabel Global bersifat bisa di akses dari bagian program mana pun dari berbagai file 
+
+$nama = 'Rafa Maritza'; // <- Variabel Global 
+echo "file-01.php <br>";
+echo "Nama : {$nama} <br> <br>";
+
+
+?>
---
+++ variabel enviroment/file-02.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+// syarat untuk menghubngkan file menggunakan require / include
+echo "file-02.php <br>";
+echo "Nama :{$nama} <br><br>";
+
+$nama .= " Sangat Baik";
+
+
+?>
