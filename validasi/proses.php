29fb5aecc4cfcaec286b308ba4aa85eca9b70dcf
rafamaritza <rafamaritza456@gmail.com>
2022-11-22 13:41:26 +0700

    passing error to url

--- validasi/proses.php
+++ validasi/proses.php
@@ -11,10 +11,24 @@ $rules = [
     'usia' => ['required','numeric']
 ];
 
+
 $errors = validasi($rules);
 
-echo "<pre>";
-print_r($errors);
-echo "</pre>";
+// echo "<pre>";
+// print_r($errors);
+// echo "</pre>";
+
+// disini kita bisa melakukan prosses yang harus dilakukan 
+// jika tidak terjadi error validasi apapun 
+
+if (count($errors) > 0){
+    $time = $_REQUEST;
+    $queryString = http_build_query([
+        'errors' => $errors,
+        'time' => $old
+    ]);
+    header("lcation: form.php?{$queryString}");
+    die();
+}
 
 ?>
